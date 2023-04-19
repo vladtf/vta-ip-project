104e34c496556e45f009b896b237e7130e05027e
AndreiCosmin27 <andreimihaicosmin@yahoo.com>
2023-04-19 21:16:04 +0300

    addLoginPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter  ,Route, Link, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Email:', email, 'Password:', password);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route exact path="/" component={Home} /> */}
+        <Route path="/login" element={<Login/>} />
+         <Route path="/*" element={<h1>Not Found</h1>} /> 
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function Login() {
+ const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Email:', email, 'Password:', password);
+  }
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
