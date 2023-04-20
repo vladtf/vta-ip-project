1195ac687d0e5bc80614540ae2c4d4d3b5d0c876
vladtf <vlad.tiftilov@yahoo.com>
2023-04-20 10:07:35 +0300

    add user repository

---
+++ backend/src/main/java/com/atv/backend/dao/repositories/UserRepository.java
@@ -0,0 +1,30 @@
+package com.atv.backend.dao.repositories;
+
+import com.atv.backend.dao.entities.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByName(String name);
+
+    @Override
+    List<User> findAll();
+
+    @Override
+    Optional<User> findById(Integer id);
+
+    @Override
+    <S extends User> S save(S entity);
+
+    @Override
+    void delete(User entity);
+
+    @Override
+    void deleteById(Integer id);
+
+    @Override
+    boolean existsById(Integer id);
+}
