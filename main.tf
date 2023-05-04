73023f6660895e4c3b0ba33222324b9973acae69
vladtf <vlad.tiftilov@yahoo.com>
2023-05-04 22:57:25 +0300

    fix credentials

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "{{ secrets.AWS_ACCESS_KEY_ID }}"
-  secret_key = "{{ secrets.AWS_SECRET_ACCESS_KEY }}"
+  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
   region     = "eu-central-1"
 }
 
