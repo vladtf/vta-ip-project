0b200da4ae270257f3106881643f55025db128ab
vladtf <vlad.tiftilov@yahoo.com>
2023-05-04 23:23:35 +0300

    add backend config

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-bucket"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
 resource "aws_ecr_repository" "backend_repository" {
   name = "backend_repository"
 }
