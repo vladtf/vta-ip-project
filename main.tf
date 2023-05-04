f3b51e3b90d8961d5c30957c2fe3d99d9b485153
vladtf <vlad.tiftilov@yahoo.com>
2023-05-04 22:10:38 +0300

    add credentials

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  access_key = "{{ secrets.AWS_ACCESS_KEY_ID }}"
+  secret_key = "{{ secrets.AWS_SECRET_ACCESS_KEY }}"
+  region     = "eu-central-1"
+}
+
 resource "aws_ecr_repository" "backend_repository" {
   name = "backend_repository"
 }
