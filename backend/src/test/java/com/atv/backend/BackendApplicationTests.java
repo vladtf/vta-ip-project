eacdb78680c29b611f45aefbbd84dad64873e16b
Vlad Tiftilov <45298740+vladtf@users.noreply.github.com>
2023-05-05 08:00:42 -0700

    test

--- backend/src/test/java/com/atv/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/atv/backend/BackendApplicationTests.java
@@ -9,7 +9,7 @@ class BackendApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		// Test if the context loadssssssssssssss
+		// Test if the context loadsssssssssssssss
 		Assert.assertTrue(true);
 	}
 }
