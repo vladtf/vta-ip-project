60d4c99f7d97c0bf6c81c98857dc2d2b9011a87a
Vlad Tiftilov <45298740+vladtf@users.noreply.github.com>
2023-05-10 13:01:39 -0700

    failing PR (#21)
    * add test scope database
    
    * mocked connection to database for unit tests
    
    * update pom version

---
+++ backend/src/main/java/com/atv/backend/config/TestDataSourceConfig.java
@@ -0,0 +1,22 @@
+package com.atv.backend.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("test")
+public class TestDataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+}
--- backend/src/test/java/com/atv/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/atv/backend/BackendApplicationTests.java
@@ -3,12 +3,14 @@ package com.atv.backend;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
-	@Test
-	public void contextLoads() {
-		Assert.assertTrue(true);
-	}
+    @Test
+    public void contextLoads() {
+        Assert.assertTrue(true);
+    }
 }
