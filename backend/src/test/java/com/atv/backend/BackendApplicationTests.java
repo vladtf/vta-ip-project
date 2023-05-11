38ca87507e04a684e65bcedb329e8c8fd1cca419
vladtf <vlad.tiftilov@yahoo.com>
2023-05-11 18:50:15 +0300

    trigger image scan

--- backend/src/test/java/com/atv/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/atv/backend/BackendApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 class BackendApplicationTests {
-
+//
     @Test
     public void contextLoads() {
         Assert.assertTrue(true);
