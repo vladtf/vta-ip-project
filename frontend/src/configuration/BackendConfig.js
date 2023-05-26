9f540aaa3a28e7d5de99bed888895c8194c5614a
vladtf <vlad.tiftilov@yahoo.com>
2023-05-26 10:44:15 +0300

    change backend url

--- frontend/src/configuration/BackendConfig.js
+++ frontend/src/configuration/BackendConfig.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://backend-service.vta-namespace:8090";
+export const BACKEND_URL = "http://vta-load-balancer-06cf52818133b134.elb.eu-central-1.amazonaws.com:8090";
 // export const BACKEND_URL = "http://localhost:8090";
