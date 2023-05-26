edf9d80bfac093b9fd7426e727b1b3680bbb8bc6
vladtf <vlad.tiftilov@yahoo.com>
2023-05-26 10:57:35 +0300

    improve the pipeline to fetch loadbalancer dns

--- frontend/src/configuration/BackendConfig.js
+++ frontend/src/configuration/BackendConfig.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://vta-load-balancer-06cf52818133b134.elb.eu-central-1.amazonaws.com:8090";
+export const BACKEND_URL = `http://${process.env.REACT_APP_LOAD_BALANCER_DNS}:8090`;
 // export const BACKEND_URL = "http://localhost:8090";
